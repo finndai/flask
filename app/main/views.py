e327050edc83cc01e8b473a8c40eef2a0f85150c
finn <finndai@163.com>
2017-06-15 20:29:03 +0800

    restful

--- app/main/views.py
+++ app/main/views.py
@@ -223,7 +223,7 @@ def moderate_enable(id):
 @login_required
 @permission_required(Permission.MODERATE_COMMENTS)
 def moderate_disable(id):
-    comment = Comment.query.get_or_404
+    comment = Comment.query.get_or_404(id)
     comment.disabled = True
     db.session.add(comment)
     return redirect(url_for('.moderate',page=request.args.get('page',1,type=int)))
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app,request
+from flask import current_app,request,url_for
 from flask_login import UserMixin,AnonymousUserMixin
 from . import db, login_manager
 from markdown import markdown
@@ -28,10 +28,10 @@ class Role(db.Model):
     def insert_roles():
         roles = {
             'User':(Permission.FOLLOW|
-                    Permission.COMMIT|
+                    Permission.COMMENT|
                     Permission.WRITE_ARTICLES,True),
             'Moderator':(Permission.FOLLOW|
-                         Permission.COMMIT|
+                         Permission.COMMENT|
                          Permission.WRITE_ARTICLES|
                          Permission.MODERATE_COMMENTS,False),
             'Administrator':(0xff,False)
@@ -105,6 +105,13 @@ class User(UserMixin, db.Model):
             except IntegrityError:
                 db.session.rollback()
 
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     def __init__(self,**kwargs):
         super(User,self).__init__(**kwargs)
@@ -115,6 +122,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        self.followed.append(Follow(followed=self))   #last time test wrong
 
     def can(self,permissions):
         return self.role is not None and \
@@ -196,7 +204,7 @@ class User(UserMixin, db.Model):
         if request.is_secure:     #判断request是否为https
             url = 'https://secure.gravatar.com/avatar'
         else:
-            url = 'https://cn.gravatar.com/avatar'
+            url = 'http://www.gravatar.com/avatar'
         hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url,hash=hash,
                                                                     size=size,default=default,rating=rating)
@@ -224,14 +232,32 @@ class User(UserMixin, db.Model):
         return Post.query.join(Follow,Follow.followed_id==Post.author_id)\
                     .filter(Follow.follower_id==self.id)
 
-    @staticmethod
-    def add_self_follows():
-        for user in User.query.all():
-            if not user.is_following(user):
-                user.follow(user)
-                db.session.add(user)
-                db.session.commit()
 
+
+    def to_json(self):
+        json_user ={
+            'url':url_for('api.get_user',id=self.id,_external=True),
+            'username':self.username,
+            'member_since':self.member_since,
+            'last_seen':self.last_seen,
+            'posts':url_for('api.get_user_posts',id=self.id,_external=True),
+            'followed_posts':url_for('api.get_user_followed_posts',id=self.id,_external=True),
+            'post_count':self.posts.count()
+        }
+        return json_user
+
+    def generate_auth_token(self,expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],expires_in=expiration)
+        return s.dumps({'id':self.id}).decode('ascii')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -276,6 +302,24 @@ class Post(db.Model):
                       'pre','strong','ul','h1','h2','h3','p']
         target.body_html = bleach.linkify(bleach.clean(markdown(value,output_format='html'),
                                                       tags=allowed_tags,strip=True))
+    def to_json(self):
+        json_post = {
+            'url':url_for('api.get_post',id=self.id,_external=True),
+            'body':self.body,
+            'body_html':self.body_html,
+            'timestamp':self.timestamp,
+            'author':url_for('api.get_user',id=self.author_id,_external=True),
+            'comments':url_for('api.get_post_comments',id=self.id,
+                               _external=True),
+            'comment_count':self.comments.count()
+        }
+        return json_post
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body =='':
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
 
 db.event.listen(Post.body,'set',Post.on_changed_body)
 
@@ -294,5 +338,21 @@ class Comment(db.Model):
         allowed_tags =['a','abbr','acronym','b','code','em','i','strong']
         target.body_html = bleach.linkify(bleach.clean(markdown(value,output_format='html'),
                                                        tags=allowed_tags,strip=True))
+    def to_json(self):
+        json_comment = {
+            'url':url_for('api.get_comment',id=self.id,_external=True),
+            'post':url_for('api.get_post',id=self.post_id,_external=True),
+            'body':self.body,
+            'body_html':self.body_html,
+            'timestamp':self.timestamp,
+            'author':url_for('api.get_user',id=self.author_id,_external=True),
+        }
+        return json_comment
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body =='':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
 
 db.event.listen(Comment.body,'set',Comment.on_changed_body)
